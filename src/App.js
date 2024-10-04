2c24b7e06932d77606e99dc12360963fcddfa084
santiagorecio <santiagorecio23@gmail.com>
2024-10-04 11:29:29 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { Cuerpo } from './components/Cuerpo.js';
 import {Body} from './components/Body.js';
 import './App.css';
 import { useState } from 'react';
-
+import {Logo} from './components/Logo.js';
 function App() {
-  
+  const[mostrarDatosPersonales, setmostrarDatosPersonales]= useState(true);
+  const handleClick = ( )=>{
+    setmostrarDatosPersonales(!mostrarDatosPersonales)
+  };
+
   return (
   <div className='cv'>
+
+    <Logo className='foto2' foto="./fotocv.jpg"/>
+
     <Header mail="santiagorecio23@gmail.com" edad="31" dni="37.018.577" fecha="31/03/1993" telefono="1155018894" ubicacion="Buenos Aires, Argentina" foto="./fotocv.jpg" linkedin='www.linkedin.com/in/santiagorecio'></Header>
-    <Cuerpo nombre="Santiago Jose Recio"/>
+    <Cuerpo  nombre="Santiago Jose Recio"/>
+    
   </div>
     
   );
--- src/components/Cuerpo.js
+++ src/components/Cuerpo.js
@@ -21,9 +21,9 @@ export const Cuerpo = (props) => {
             <Educacion estudio="Diplomatura en Python" institucion= "U.T.N" situacion= "Finalizado"/>
             <Educacion estudio="Desarrollador WEB en HTML y CSS" institucion= "CODER HOUSE" situacion= "Finalizado"/>
             <Educacion estudio="Curso en JAVASCRIPT y REACT " institucion= "CODER HOUSE" situacion= "Finalizado"/>
-            <Educacion estudio="Licenciatura en Ciencia de Datos" institucion=" Universidad de Buenos Aires" situacion=" 3 año"/>
-            <Educacion estudio="Licenciatura en Matematica Aplicada" institucion=" Universidad de Buenos Aires" situacion=" 2 año"/>
-            <Educacion estudio= "Contabilidad" institucion= "Universidad de Buenos Aires" situacion= "4 año - Abandonado"/>
+            <Educacion estudio="Licenciatura en Ciencia de Datos" institucion=" Universidad de Buenos Aires" situacion=" 3° Año"/>
+            <Educacion estudio="Licenciatura en Matematica Aplicada" institucion=" Universidad de Buenos Aires" situacion=" 2° Año"/>
+            <Educacion estudio= "Contabilidad" institucion= "Universidad de Buenos Aires" situacion= " Cursado hasta 4° Año"/>
             
         </div>
 }
