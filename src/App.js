c0151acac8468f0dc8a342de8eee2d64727d7318
santiagorecio <santiagorecio23@gmail.com>
2024-10-04 11:44:46 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ function App() {
 
   return (
   <div className='cv'>
-
-    <Logo className='foto2' foto="./fotocv.jpg"/>
-
     <Header mail="santiagorecio23@gmail.com" edad="31" dni="37.018.577" fecha="31/03/1993" telefono="1155018894" ubicacion="Buenos Aires, Argentina" foto="/fotocv.jpg" linkedin='www.linkedin.com/in/santiagorecio'></Header>
     <Cuerpo  nombre="Santiago Jose Recio"/>
     
--- src/components/DatosPersonales.js
+++ src/components/DatosPersonales.js
@@ -13,8 +13,8 @@ export const DatosPersonales = (props) =>{
         </div>
         </div>
         <a className="dato telefono" href="https://wa.me/5491155018894" target="_blank"> <img src='llamada.png'></img>+54 {telefono}</a>
-        <h4 className="dato ubicacion" ><img src="/ubicacion.png" alt=""/>{ubicacion}</h4>
-        <a className='dato mail' href='mailto:santiagorecio23@gmail.com' target="_blank" ><img src="/mail.png"/>{mail}</a> 
+        <h4 className="dato ubicacion" ><img src="ubicacion.png" alt=""/>{ubicacion}</h4>
+        <a className='dato mail' href='mailto:santiagorecio23@gmail.com' target="_blank" ><img src="mail.png"/>{mail}</a> 
         <h4>
         <a className='dato linkedin' href='https://www.linkedin.com/in/santiago-recio-59716392' target="_blank" ><img src="linkedin.png"/>{linkedin}</a>
         </h4>
